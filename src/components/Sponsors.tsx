c9df3ff603220de4e0e636e6bcbce4314d273e46
Keenan Karich <keenanmkarich@gmail.com>
2024-07-03 16:21:56 -0500

    update

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -23,7 +23,7 @@ export function Sponsors() {
     <section id="sponsors" aria-label="Sponsors" className="py-20 sm:py-32">
       <Container>
         <h2 className="mx-auto max-w-2xl text-center font-display text-4xl font-medium tracking-tighter text-blue-900 sm:text-5xl">
-          Trusted By{' '}
+          Trusted By
         </h2>
         <div className="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-3 md:gap-x-16 lg:gap-x-32">
           {sponsors.map((sponsor) => (
@@ -35,7 +35,7 @@ export function Sponsors() {
                 src={sponsor.logo}
                 alt={sponsor.name}
                 unoptimized
-                className="grayscale"
+                className="rounded-2xl grayscale"
               />
             </div>
           ))}
